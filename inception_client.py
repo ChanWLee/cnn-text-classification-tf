226b6041ce92ef37fc20e7d6a5dfe4b89c60b0ab
chanwoo.lee <cwl@bpuholdings.com>
2017-11-22 13:36:39 +0900

    success on serving

--- inception_client.py
+++ inception_client.py
@@ -2,50 +2,58 @@
 
 from __future__ import print_function
 
-# This is a placeholder for a Google-internal import.
-
-from grpc.beta import implementations
 import tensorflow as tf
-
+from grpc.beta import implementations
 from tensorflow_serving.apis import predict_pb2
 from tensorflow_serving.apis import prediction_service_pb2
+from tensorflow.contrib import learn
+import numpy as np
 
+# This is a placeholder for a Google-internal import.
 
 tf.app.flags.DEFINE_string('server', 'localhost:9000',
                            'PredictionService host:port')
 tf.app.flags.DEFINE_string('text', '학교에 가기 싫어요', 'text')
 tf.app.flags.DEFINE_string('file', '', 'path to text file')
+tf.app.flags.DEFINE_string('vocab', '', 'path to vocab file')
 FLAGS = tf.app.flags.FLAGS
 
 
-def main(_):
-  host, port = FLAGS.server.split(':')
-  channel = implementations.insecure_channel(host, int(port))
-  stub = prediction_service_pb2.beta_create_PredictionService_stub(channel)
-  if FLAGS.file:
-    with open(FLAGS.file, 'rb') as f:
-      # See prediction_service.proto for gRPC request/response details.
-      data = f.read()
-      request = predict_pb2.PredictRequest()
-      request.model_spec.name = 'inception'
-      request.model_spec.signature_name = 'predict_images'
-      request.inputs['images'].CopyFrom(
-          tf.contrib.util.make_tensor_proto(data, shape=[1]))
-      result = stub.Predict(request, 10.0)  # 10 secs timeout
-      print(result)
+def transform_vocab(vocab_path, data):
+    vocab_processor = learn.preprocessing.VocabularyProcessor.restore(vocab_path)
+    return np.array(list(vocab_processor.transform(data)))
 
-  if FLAGS.text:
-    data = FLAGS.text
-    print('data:{}'.format(data))
-  # data = '학교에 가기 싫어'
-    request = predict_pb2.PredictRequest()
-    request.model_spec.name = 'inception'
-    request.model_spec.signature_name = 'predict_images'
-    request.inputs['images'].CopyFrom(
-        tf.contrib.util.make_tensor_proto(data, shape=[1]))
-    result = stub.Predict(request, 10.0)  # 10 secs timeout
-    print('result:{}'.format(result))
+def main(_):
+    host, port = FLAGS.server.split(':')
+    channel = implementations.insecure_channel(host, int(port))
+    stub = prediction_service_pb2.beta_create_PredictionService_stub(channel)
+    if FLAGS.file:
+        with open(FLAGS.file, 'rb') as f:
+            data = []
+            data_list = []
+            for line in f.readlines():
+                data += line.strip().split(',')
+                data_list.append(line)
+            x_test = data
+            if FLAGS.vocab:
+                x_test = transform_vocab(FLAGS.vocab, data)
+            tf_data = [int(i) for i in x_test]
+            leng_data = len(data_list)
+
+            request = predict_pb2.PredictRequest()
+            request.model_spec.name = 'inception'
+            request.model_spec.signature_name = 'predict_sentiment_score'
+            # request.inputs['rawx'].CopyFrom(
+            #     tf.contrib.util.make_tensor_proto(data, shape=[leng_data, 68]))
+            request.inputs['inputx'].CopyFrom(
+                tf.contrib.util.make_tensor_proto(tf_data, shape=[leng_data, 68]))
+            request.inputs['dropout'].CopyFrom(
+                tf.contrib.util.make_tensor_proto(1.0, shape=[1]))
+            request.inputs['phase_train'].CopyFrom(
+                tf.contrib.util.make_tensor_proto(False, dtype=tf.bool))
+            result = stub.Predict(request, 10.0)  # 10 secs timeout
+            print(result)
 
 if __name__ == '__main__':
-  tf.app.run()
-  # main()
+    tf.app.run()
+    # main()
--- serving.py
+++ serving.py
@@ -22,17 +22,17 @@ data_loader.define_flags()
 
 FLAGS = tf.flags.FLAGS
 FLAGS._parse_flags()
-print("\nParameters:")
-for attr, value in sorted(FLAGS.__flags.items()):
-    print("{}={}".format(attr.upper(), value))
-print("")
-
-if FLAGS.eval_train:
-    x_raw, y_test = data_loader.load_data_and_labels()
-    y_test = np.argmax(y_test, axis=1)
-else:
-    x_raw, y_test = data_loader.load_dev_data_and_labels()
-    y_test = np.argmax(y_test, axis=1)
+# print("\nParameters:")
+# for attr, value in sorted(FLAGS.__flags.items()):
+#     print("{}={}".format(attr.upper(), value))
+# print("")
+#
+# if FLAGS.eval_train:
+#     x_raw, y_test = data_loader.load_data_and_labels()
+#     y_test = np.argmax(y_test, axis=1)
+# else:
+#     x_raw, y_test = data_loader.load_dev_data_and_labels()
+#     y_test = np.argmax(y_test, axis=1)
 
 # checkpoint_dir이 없다면 가장 최근 dir 추출하여 셋팅
 if FLAGS.checkpoint_dir == "":
@@ -41,10 +41,10 @@ if FLAGS.checkpoint_dir == "":
     FLAGS.sub_dir = latest_subdir
     FLAGS.checkpoint_dir = latest_subdir + "/checkpoints/"
 
-# Map data into vocabulary
-vocab_path = os.path.join(FLAGS.checkpoint_dir, "..", "vocab")
-vocab_processor = data_loader.restore_vocab_processor(vocab_path)
-x_test = np.array(list(vocab_processor.transform(x_raw)))
+# # Map data into vocabulary
+# vocab_path = os.path.join(FLAGS.checkpoint_dir, "..", "vocab")
+# vocab_processor = data_loader.restore_vocab_processor(vocab_path)
+# x_test = np.array(list(vocab_processor.transform(x_raw)))
 
 # ==================================================
 checkpoint_file = tf.train.latest_checkpoint(FLAGS.checkpoint_dir)
@@ -55,8 +55,6 @@ with graph.as_default():
         log_device_placement=FLAGS.log_device_placement)
     sess = tf.Session(config=session_conf)
     with sess.as_default():
-        # export_path = os.path.join(
-        #     tf.compat.as_bytes(export_path_base))
         export_path = os.path.abspath(os.path.join(FLAGS.sub_dir, "serving", str(FLAGS.model_version)))
         print('Exporting trained model to \n', export_path)
         builder = tf.saved_model.builder.SavedModelBuilder(export_path)
@@ -66,69 +64,58 @@ with graph.as_default():
 
         input_x = graph.get_operation_by_name("input_x").outputs[0]
         input_y = graph.get_operation_by_name("input_y").outputs[0]
-        predictions = graph.get_operation_by_name("output/predictions").outputs[0]
-
         dropout_keep_prob = graph.get_operation_by_name("dropout_keep_prob").outputs[0]
         phase_train = graph.get_operation_by_name("phase_train").outputs[0]
+        predictions = graph.get_operation_by_name("output/predictions").outputs[0]
 
-        batches = data_helpers.batch_iter(list(x_test), FLAGS.batch_size, 1, shuffle=False)
-
-        all_predictions = []
-
-        for x_test_batch in batches:
-            batch_predictions = sess.run(predictions,
-                                         {input_x: x_test_batch, dropout_keep_prob: 1.0, phase_train: False})
-            all_predictions = np.concatenate([all_predictions, batch_predictions])
-
-        # class_predictions = data_loader.class_labels(all_predictions.astype(int))
-
-        # serialized_tf_example = tf.placeholder(tf.string, x_test)
-        serialized_tf_example = input_x
-        values, idx = tf.nn.top_k(all_predictions, len(all_predictions))
-        # nparr_x_test = np.array(x_test)
-        class_tensor = tf.constant(x_raw)
-
-        table = tf.contrib.lookup.index_to_string_table_from_tensor(class_tensor)
-        classes = table.lookup(tf.to_int64(idx))
-
-        # classes = tf.placeholder(dtype=tf.string, shape=[], name=table)
-        # classes = table
-        # values = all_predictions
+        # input_rawx = tf.placeholder(tf.string, shape=([],68))
 
 
-
-        # {input_x: x_test, input_y: y_test, predictions: all_predictions}
-        classification_inputs = tf.saved_model.utils.build_tensor_info(
-            serialized_tf_example
-        )
-        classification_outputs_classes = tf.saved_model.utils.build_tensor_info(
-            classes
-        )
-        classification_outputs_scores = tf.saved_model.utils.build_tensor_info(
-            values
-        )
-
-        classification_signature = (
-            tf.saved_model.signature_def_utils.build_signature_def(
-                inputs={
-                    tf.saved_model.signature_constants.CLASSIFY_INPUTS:
-                        classification_inputs
-                },
-                outputs={
-                    tf.saved_model.signature_constants.CLASSIFY_OUTPUT_CLASSES:
-                        classification_outputs_classes,
-                    tf.saved_model.signature_constants.CLASSIFY_OUTPUT_SCORES:
-                        classification_outputs_scores
-                },
-                method_name=tf.saved_model.signature_constants.CLASSIFY_METHOD_NAME))
+        # classify_inputs_x = tf.saved_model.utils.build_tensor_info(
+        #     input_x
+        # )
+        # classify_input_dropout = tf.saved_model.utils.build_tensor_info(
+        #     dropout_keep_prob
+        # )
+        # classify_input_phase_train = tf.saved_model.utils.build_tensor_info(
+        #     phase_train
+        # )
+        # prediction_output_tensor_info = tf.saved_model.utils.build_tensor_info(
+        #     predictions
+        # )
+        #
+        # classification_signature = (
+        #     tf.saved_model.signature_def_utils.build_signature_def(
+        #         inputs={
+        #             tf.saved_model.signature_constants.CLASSIFY_INPUTS_X:
+        #                 classify_inputs_x,
+        #             tf.saved_model.signature_constants.CLASSIFY_INPUT_DROPOUT:
+        #                 classify_input_dropout,
+        #             tf.saved_model.signature_constants.CLASSIFY_INPUT_PHASE_TRAIN:
+        #                 classify_input_phase_train
+        #         },
+        #         outputs={
+        #             # tf.saved_model.signature_constants.CLASSIFY_OUTPUT_CLASSES:
+        #             #     classification_outputs_classes,
+        #             tf.saved_model.signature_constants.PREDICTION_OUTPUT_TENSOR_INFO:
+        #                 prediction_output_tensor_info
+        #         },
+        #         method_name=tf.saved_model.signature_constants.CLASSIFY_METHOD_NAME))
 
         tensor_info_x = tf.saved_model.utils.build_tensor_info(input_x)
-        tensor_info_y = tf.saved_model.utils.build_tensor_info(input_y)
+        # tensor_info_rawx = tf.saved_model.utils.build_tensor_info(input_rawx)
+        tensor_info_dropout = tf.saved_model.utils.build_tensor_info(dropout_keep_prob)
+        tensor_info_prediction = tf.saved_model.utils.build_tensor_info(predictions)
+        tensor_info_phase_train = tf.saved_model.utils.build_tensor_info(phase_train)
 
         prediction_signature = (
             tf.saved_model.signature_def_utils.build_signature_def(
-                inputs={'images': tensor_info_x},
-                outputs={'scores': tensor_info_y},
+                inputs={'inputx': tensor_info_x,
+                        'dropout': tensor_info_dropout,
+                        'phase_train': tensor_info_phase_train},
+                outputs={
+                    # 'rawx': tensor_info_rawx,
+                         'prediction': tensor_info_prediction},
                 # method_name="positive_negative"))
                 method_name=tf.saved_model.signature_constants.PREDICT_METHOD_NAME))
 
@@ -137,10 +124,10 @@ with graph.as_default():
         builder.add_meta_graph_and_variables(
             sess, [tf.saved_model.tag_constants.SERVING],
             signature_def_map={
-                'predict_images':
+                'predict_sentiment_score':
                     prediction_signature,
-                tf.saved_model.signature_constants.DEFAULT_SERVING_SIGNATURE_DEF_KEY:
-                    classification_signature,
+                # tf.saved_model.signature_constants.DEFAULT_SERVING_SIGNATURE_DEF_KEY:
+                #     classification_signature,
             },
             legacy_init_op=legacy_init_op)
 
