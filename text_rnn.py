bab4d7e01435a1bdfc75c01f29a468583dd03865
chanwoo.lee <cwl@bpuholdings.com>
2018-05-08 16:36:28 +0900

    rnn 수정중.....에러

--- text_rnn.py
+++ text_rnn.py
@@ -5,56 +5,63 @@ class TextRNN(object):
 
     def __init__(self, batch_normalization, sequence_length, num_classes, vocab_size,
             embedding_size, filter_sizes, num_filters, l2_reg_lambda=0.0):
+        embedding_size =2
+        print('sl={}, nc={},vx={},es={},fs={},nf={}'.format(sequence_length, num_classes, vocab_size,embedding_size, len(filter_sizes), num_filters))
 
         hidden_dim = num_filters
+        batch_size = 64
         # Placeholders for input, output and dropout
         self.input_x = tf.placeholder(tf.int32, [None, sequence_length], name="input_x")
         self.input_y = tf.placeholder(tf.float32, [None, num_classes], name="input_y")
         self.dropout_keep_prob = tf.placeholder(tf.float32, name="dropout_keep_prob")
         self.phase_train = tf.placeholder(tf.bool, name='phase_train')
 
-        # x,y one_hot encoding
-        x_one_hot = tf.one_hot(self.input_x, sequence_length)
 
         with tf.device('/cpu:0'), tf.name_scope("embedding"):
-            W = tf.Variable(
-                tf.random_uniform([vocab_size, embedding_size], -1.0, 1.0),
-                name="W")
+
+            W = tf.get_variable(
+                "W",
+                shape=[hidden_dim, num_classes],
+                initializer=tf.contrib.layers.xavier_initializer())
+            b = tf.Variable(tf.constant(0.1, shape=[num_classes]), name="b")
+            w = tf.get_variable("w",[hidden_dim, num_classes])
             self.embedded_chars = tf.nn.embedding_lookup(W, self.input_x)
             self.embedded_chars_expanded = tf.expand_dims(self.embedded_chars, -1)
 
-        with tf.variable_scope('rnnlm'):
-            softmax_w = tf.get_variable("softmax_w", [hidden_dim, vocab_size])
-            sotfmax_b = tf.get_variable("sotfmax_b", [vocab_size])
-
+        
         def lstm_cell():
             lstm = rnn.BasicLSTMCell(num_units=hidden_dim, state_is_tuple=True)
-            return lstm
-            #return tf.contrib.rnn.DropoutWrapper(lstm, output_keep_prob=self.dropout_keep_prob)
+            #return lstm
+            return tf.contrib.rnn.DropoutWrapper(lstm, output_keep_prob=self.dropout_keep_prob)
 
-        multi_cells = rnn.MultiRNNCell([lstm_cell() for _ in range(len(filter_sizes))], state_is_tuple=True)
-        #initial_state = multi_cells.zero_state(filter_sizes, tf.float32)
-        initial_state = multi_cells.zero_state(64, tf.float32)
+        multi_cells = rnn.MultiRNNCell([lstm_cell() for _ in range(embedding_size)], state_is_tuple=True)
+        initial_state = multi_cells.zero_state(batch_size, tf.float32)
+        
+        rnn_inputs = [tf.squeeze(i, axis=[1]) for i in tf.split(self.embedded_chars, sequence_length, 1)]
 
-        rnn_inputs = [tf.squeeze(i, axis=[1]) for i in tf.split(x_one_hot, sequence_length, 1)]
-        outputs, _state = tf.contrib.rnn.static_rnn(multi_cells, rnn_inputs, initial_state=initial_state )
+        def loop(prev, _):
+            prev = tf.matmul(prev, W) + b
+            prev_symbol = tf.stop_gradient(tf.argmax(prev, 1))
+            return tf.nn.embedding_lookup(W, prev_symbol)
+
+        #rnn_inputs = [i for i in tf.split(x_one_hot, sequence_length, 1)]
+        #outputs, _state = tf.contrib.rnn.static_rnn(multi_cells, rnn_inputs, initial_state=initial_state )
         #outputs, _state = tf.nn.dynamic_rnn(multi_cells, rnn_inputs, dtype=tf.float32)
+        outputs, _state = tf.contrib.legacy_seq2seq.rnn_decoder(rnn_inputs, initial_state, multi_cells
+                #,loop_function=loop
+                ,scope='rnnlm')
 
         seq_output = tf.concat(outputs, axis=1)
         #output = tf.reshape(seq_output, [-1, hidden_dim])
         output = seq_output
 
-        with tf.variable_scope('softmax'):
-            softmax_w = tf.Variable(tf.truncated_normal((hidden_dim, num_classes), stddev=0.1))
-            softmax_b = tf.Variable(tf.zeros(num_classes))
-
         with tf.name_scope("output"):
-            W = tf.get_variable(
-                "W",
+            W1 = tf.get_variable(
+                "W1",
                 shape=[num_filters*sequence_length, num_classes],
                 initializer=tf.contrib.layers.xavier_initializer())
-            b = tf.Variable(tf.constant(0.1, shape=[num_classes]), name="b")
-            preds = self.scores = tf.nn.xw_plus_b(output, W, b, name="scores")
+            #b = tf.Variable(tf.constant(0.1, shape=[num_classes]), name="b")
+            preds = self.scores = tf.nn.xw_plus_b(output, W1, b, name="scores")
             #logits = tf.matmul(output, softmax_w) + softmax_b
             #preds = tf.nn.softmax(logits)
             self.predictions = tf.argmax(preds, 1, name="predictions")
